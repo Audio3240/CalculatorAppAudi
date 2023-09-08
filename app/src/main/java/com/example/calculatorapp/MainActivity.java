90b1d9795915c793e56bb11033e64808c3d61a6c
Audi T <113703273+Audio3240@users.noreply.github.com>
2023-09-08 11:57:58 -0500

    Created numberStyle and numSelected function

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int numInput;
+    public void numSelected(View v){
+        if(v.getId()==R.id.b0){
+            numInput = 0;
+            Log.i("Trairat", "0 Clicked");
+        }
     }
 }
